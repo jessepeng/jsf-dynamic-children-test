bcf01ad176a8917c0d43fe0c760a5c5d02088272
Jan-Christopher <jan-christopher.pien@lhind.dlh.de>
2017-02-10 16:38:00 +0100

    Update names

--- src/main/java/de/lhind/example/BasicBean.java
+++ src/main/java/de/lhind/example/BasicBean.java
@@ -9,53 +9,53 @@ public class BasicBean implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	private String column1;
+	private String title;
 	
-	private String column2;
+	private String label;
 	
-	private String column3;
+	private String value;
 	
-	private boolean column4;
+	private boolean additional;
 
 	public BasicBean() {	
 	}
 
 	public BasicBean(String column1, String column2, String column3, boolean column4) {
-		this.column1 = column1;
-		this.column2 = column2;
-		this.column3 = column3;
-		this.column4 = column4;
+		this.title = column1;
+		this.label = column2;
+		this.value = column3;
+		this.additional = column4;
 	}
 	
-	public String getColumn1() {
-		return column1;
+	public String getTitle() {
+		return title;
 	}
 
-	public void setColumn1(String column1) {
-		this.column1 = column1;
+	public void setTitle(String column1) {
+		this.title = column1;
 	}
 
-	public String getColumn2() {
-		return column2;
+	public String getLabel() {
+		return label;
 	}
 
-	public void setColumn2(String column2) {
-		this.column2 = column2;
+	public void setLabel(String column2) {
+		this.label = column2;
 	}
 
-	public String getColumn3() {
-		return column3;
+	public String getValue() {
+		return value;
 	}
 
-	public void setColumn3(String column3) {
-		this.column3 = column3;
+	public void setValue(String column3) {
+		this.value = column3;
 	}
 
-	public boolean isColumn4() {
-		return column4;
+	public boolean isAdditional() {
+		return additional;
 	}
 
-	public void setColumn4(boolean column4) {
-		this.column4 = column4;
+	public void setAdditional(boolean column4) {
+		this.additional = column4;
 	}
 }
